658be7dbb6bd492e82e9d5d55c768a3c782d6120
knuevena <knuevenam@gmail.com>
2016-03-31 12:11:03 -0400

    move db setup back into db.py

--- db.py
+++ db.py
@@ -1,10 +1,14 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relation, sessionmaker
 from sqlalchemy import create_engine
+import main
 
 Base = declarative_base()
 #replace with config setting for database
 import config
 database_engine = create_engine(config.database_config)
 Session = sessionmaker(bind=database_engine)
+
+if __name__ == '__main__':
+    main.setup()
 
