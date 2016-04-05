f200e7e2fa79943e108824e5f7829a8d77482ecf
Olivia DeFlumeri <livd1125@gmail.com>
2016-04-05 16:31:52 -0400

    returning json token from login

--- views.py
+++ views.py
@@ -9,21 +9,26 @@ from flask import Flask, request, jsonify, session
 from db import Base, Session
 from app import app
 from user import User
+import jwt
+from datetime import datetime, timedelta
 @app.route('/')
 def index():
     content = {'test content':'disregard'}
     return content, status.HTTP_404_NOT_FOUND
 
 @app.route('/user', methods=['GET', 'POST', 'PUT', 'DELETE'])
-def user():
+def users():
     content = {'content':'blah'}
     success = {'status':'account created, yay!'}
     error = {'error': 'Error in JSON/SQL syntax'}
+    error2 = {'error': 'User already exists'}
     if request.method == 'POST':
         data = request.json
         if data['permissions'] == 'volunteer':
             if volunteer.Volunteer.createVolunteer(data):
                 return success, status.HTTP_200_OK
+            else: 
+                return error2, status.HTTP_200_OK
         if data['permissions'] == 'admin':
             if admin.Admin.createAdmin(data):
                 return success, status.HTTP_200_OK
@@ -59,9 +64,28 @@ def login():
     if user and user.check_password(json_data['passwordhash']):
         session['logged_in'] = True
         status = True
+        return create_token(user)
     else:
         status = False
-    return jsonify({'result': status})
+        return jsonify({'result': status})
+
+def create_token(user):
+    payload = {
+        # subject
+        'sub': user.id,
+        #issued at
+        'iat': datetime.utcnow(),
+        #expiry
+        'exp': datetime.utcnow() + timedelta(days=1)
+    }
+ 
+    token = jwt.encode(payload, app.secret_key, algorithm='HS256')
+    return token
+
+
+def parse_token(req):
+    token = req.headers.get('Authorization').split()[1]
+    return jwt.decode(token, app.secret_key, algorithms='HS256')
 
 @app.route('/logout', methods=['GET'])
 def logout():
