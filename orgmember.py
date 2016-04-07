e477ce06c3e50de0b26002d6e233a84cb481af9c
joeyw526 <josephmwood@Josephs-MacBook-Pro.local>
2016-04-07 15:33:02 -0400

    Org member field changes
    Removed extra fields that are no longer necessary, will probably break
    some tests, editing those now

--- orgmember.py
+++ orgmember.py
@@ -11,20 +11,13 @@ class OrgMember(User):
     __mapper_args__ = {'polymorphic_identity': 'orgmember'}
     id = Column(Integer, ForeignKey('users.id'), primary_key=True, nullable=False)
     # the OrgMember will have all User fields
-    vhours = Column(Integer)  # will be a seperate table later, could be merged into events
-    neighborhood = Column(String(255))  # seperate table Neighborhoods
-    interests = Column(String(255))  # enum?
-    education = Column(String(255))  # seperate table
-    availability = Column(String(255))  # this will need some discussion
-    events = Column(String(255))  # will need to foreignkey to another table later
     org = Column(Integer, ForeignKey('organizations.id'))  # object or id?
     poc = Column(Boolean, nullable=False)
 
     @classmethod
     def fromdict(cls, d):
         allowed = ('name', 'email', 'passwordhash', 'phone', 'last_active', 'birthdate',
-                   'bio', 'gender', 'vhours', 'neighborhood', 'interests',
-                   'education', 'availabilty', 'events', 'org', 'poc')
+                   'bio', 'gender', 'org', 'poc')
         df = {k: v for k, v in d.items() if k in allowed}
         return cls(**df)
 
@@ -34,25 +27,18 @@ class OrgMember(User):
                 dict_[key] = getattr(self, key)
         return dict_
 
-    def __init__(self, name, email, passwordhash, phone, last_active=datetime.now(), birthdate=None,
-                 bio=None, gender=None, vhours=None, neighborhood=None, interests=None,
-                 education=None, availability=None, events=None, org=None, poc=None):
+    def __init__(self, name, email, passwordhash, phone, poc, birthdate=None,
+                 bio=None, gender=None, org=None):
         self.name = name
         self.email = email
         self.set_password(passwordhash)
         self.phone = phone
-        self.last_active = last_active
+        self.poc = poc
+        self.last_active = datetime.now()
         self.birthdate = birthdate
         self.bio = bio
         self.gender = gender
-        self.vhours = vhours
-        self.neighborhood = neighborhood
-        self.interests = interests
-        self.education = education
-        self.availability = availability
-        self.events = events
         self.org = org
-        self.poc = poc
 
     def set_password(self, password):
         self.passwordhash = generate_password_hash(password)
