c090a615d6d3d95d5d22fd7c5eb622af6387b212
Olivia DeFlumeri <livd1125@gmail.com>
2016-04-19 12:40:53 -0400

    maybe this will work

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 from flask.ext.api import FlaskAPI, status
+from flask.ext.cors import CORS
 
 
 #when we have secret keys and such turn this back on
@@ -7,5 +8,7 @@ import config
 
 app = FlaskAPI(__name__)
 app.config['SECRET_KEY'] = 'super-secret'
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 # will pull config options from config.py
 #app.config.from_object(config)
